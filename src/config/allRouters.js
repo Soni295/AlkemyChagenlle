40453d80784aff3250ccb38d8dc99ee2e284b117
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-18 19:56:55 -0300

    simplify the logic of routers

--- src/config/allRouters.js
+++ src/config/allRouters.js
@@ -5,10 +5,10 @@ import { UpdatePage } from '../pages/UpdatePage'
 import { PATH } from './PATH'
 
 class RouteObj {
-  constructor(path, Component, autho=true, exact=true){
+  constructor(path, component, autho=true, exact=true){
     this.path = path.path
     this.key = path.path
-    this.Component = Component
+    this.component = component
     this.autho = autho
     this.exact = exact
   }
@@ -20,23 +20,15 @@ export const allRouters = [
   new RouteObj(PATH.LoginPage, LoginPage, false),
 ]
 
-
-export const Route = ({path, exact, Component, autho})=> {
+export const Route = ({autho, ...rest}) => {
 
   const token = localStorage.getItem('token')
 
   if(autho && !token)
     return <Redirect to={PATH.LoginPage.path} />
 
-  if(token && path === PATH.LoginPage.path)
+  if(token && rest.path === PATH.LoginPage.path)
     return <Redirect to={PATH.Home.path} />
 
-  return (
-    <ReactRoute
-      path={path}
-      key={path}
-      exact={exact}
-      component={Component}
-    />
-  )
+  return  <ReactRoute {...rest} />
 }
