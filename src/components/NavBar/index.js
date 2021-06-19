0e24a1f7c99f40ffd5097cac2fcf41f185a4f055
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-19 20:57:29 -0300

    erase navbar in the login

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,15 @@
-import { Link } from '../Link/index'
+import { useContext } from 'react'
 import { Navbar as NavbarBs } from 'react-bootstrap'
+import { UserContext } from '../../context/UserContext'
+import { Link } from '../Link/index'
 import { PATH } from '../../config/PATH'
 
+
 export const Navbar = () => {
+  const {session} = useContext(UserContext)
+
+  if(!session.token) return <></>
+
   return(
     <NavbarBs className='navbar-dark' bg='dark' expand='lg'>
       <Link page={PATH.LoginPage}/>
