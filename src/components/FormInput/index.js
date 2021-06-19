9ae8828c7d209254e32ced4e015014de19307278
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-19 19:42:55 -0300

    add loginForm

---
+++ src/components/FormInput/index.js
@@ -0,0 +1,45 @@
+import { Form } from 'react-bootstrap'
+
+/*
+export const Input = ({name, placeholder, type, control, children}) => (
+  <Form.Group controlId={name}>
+    <Form.Control
+      data-testid={name}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      onChange={control.handleChange}
+      value={control.values[name]}
+      onBlur={control.handleBlur}
+    />
+    {children}
+    {control.errors[name] && control.touched[name]
+      ?
+        <Form.Text className="text-muted error">
+          {control.errors[name]}
+        </Form.Text>
+      : null
+    }
+  </Form.Group>
+)
+*/
+
+export class InputGenerator {
+  constructor(name, placeholder, ctrl, type=name){
+   this.name = name
+   this.type = type
+   this.placeholder = placeholder
+   this.control = ctrl
+  }
+}
+
+export const Input = ({name, placeholder, type, control}) => (
+  <Form.Group controlId={name}>
+    <Form.Control
+      data-testid={name}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+    />
+  </Form.Group>
+)
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,10 +1,24 @@
+import { Form, Button } from 'react-bootstrap'
+import { Input } from '../FormInput/index'
+import { inputs } from './inputs'
 import './style.css'
 
 export const LoginForm = () => {
-  return (
+
+  return(
     <main id='bg-login'>
       <div className='login-box'>
         <h2>Login Form</h2>
+
+        <Form onSubmit={() => console.log('hola')}>
+          {inputs.map(input =>
+            <Input {...input} key={input.name}/>
+          )}
+
+          <Button variant='primary' type='submit'>
+            Send
+          </Button>
+        </Form>
       </div>
     </main>
   )
