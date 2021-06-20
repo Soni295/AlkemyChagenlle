f04f5ad431b9693c136858d2aac8149047e56a21
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-06-19 22:18:30 -0300

    add JSONPlaceHolder api

--- src/config/Apis.js
+++ src/config/Apis.js
@@ -1,3 +1,6 @@
 import axios from 'axios'
 const baseURL = 'http://challenge-react.alkemy.org'
+const baseURLJPH = 'https://jsonplaceholder.typicode.com/posts'
+
 export const LoginApi = axios.create({baseURL})
+export const JPHApi = axios.create({baseURLJPH})
